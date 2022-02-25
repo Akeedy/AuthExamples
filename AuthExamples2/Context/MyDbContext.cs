0018a5cb2fe9d521724b91e85cfd29b0af08c001
akeedy <mazlum.1472@gmail.com>
2022-02-25 19:59:10 +0300

    IdentityUser and IdentityRole Auth

---
+++ AuthExamples2/Context/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthExamples2.Context
+{
+    //IdentityDBContext all user tables
+    public class MyDbContext:IdentityDbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options):base(options)
+        {
+
+
+        }
+    }
+}
---
+++ AuthExamples2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username,string password)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(string username,string password)
+        {
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public IActionResult Secure()
+        {
+            return View(); 
+        }
+    }
+}
